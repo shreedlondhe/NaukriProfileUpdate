a8aab2c371f62c6c2bef8bf4445507d791eb8e22
shreedlondhe <shreedlondhe@gmail.com>
2026-06-19 17:56:09 +0530

    addedall detrails in .env file

--- pages/ProfilePage.ts
+++ pages/ProfilePage.ts
@@ -3,15 +3,15 @@ import Utils from "../utils/utils";
 import { log } from "../utils/Logs";
 
 export default  class ProfilePage {
-filePath:string=`C:\\Users\\DELL\\Desktop\\resume\\SDET 6 yrs Immediate joiner.pdf`;
+//filePath:string=`C:\\Users\\DELL\\Desktop\\resume\\SDET 6 yrs Immediate joiner.pdf`;
 viewProfile:Locator;
 Resumeheadline:Locator;
 resumeHeadlineTxt:Locator;
 saveButton:Locator; 
-ResumeText:string=`Dynamic Senior Test Engineer specializing in Selenium, Playwright, and API Testing, with proven expertise in automation frameworks, database validation, and CI/CD integration, committed to enhancing software quality and testing efficiency`
+//ResumeText:string=`Dynamic Senior Test Engineer specializing in Selenium, Playwright, and API Testing, with proven expertise in automation frameworks, database validation, and CI/CD integration, committed to enhancing software quality and testing efficiency`
 editProfileButton:Locator;
 profileHeadlineTxt:Locator;
-profileText=`Results-driven Software Test Engineer with over 6.6+ years of comprehensive experience in Manual and Automation Testing, specializing in developing robust automation frameworks using Selenium and Playwright with AI. Currently serving as a Senior Test Engineer at Xoriant Solutions, I have demonstrated proficiency in executing complex testing scenarios, including API, UI, and database testing, while enhancing test efficiency through innovative script generation tools like Agentic AI. I possess a strong track record in optimizing regression suites, significantly reducing testing effort, and integrating automation into CI/CD pipelines with Jenkins in Agile environments. My technical acumen includes expertise in TypeScript, Postman, and AWS, coupled with a solid foundation in civil engineering, making me adept at maintaining high-quality standards in software delivery.`
+//profileText=`Results-driven Software Test Engineer with over 6.6+ years of comprehensive experience in Manual and Automation Testing, specializing in developing robust automation frameworks using Selenium and Playwright with AI. Currently serving as a Senior Test Engineer at Xoriant Solutions, I have demonstrated proficiency in executing complex testing scenarios, including API, UI, and database testing, while enhancing test efficiency through innovative script generation tools like Agentic AI. I possess a strong track record in optimizing regression suites, significantly reducing testing effort, and integrating automation into CI/CD pipelines with Jenkins in Agile environments. My technical acumen includes expertise in TypeScript, Postman, and AWS, coupled with a solid foundation in civil engineering, making me adept at maintaining high-quality standards in software delivery.`
 
 constructor(private page: Page){ 
     this.viewProfile=page.locator(".view-profile-wrapper>a");
@@ -75,7 +75,7 @@ async updateResume(){
         
      this.page.getByRole('button', { name: 'Update resume' }).click()
 ]);
-     await fileChooser.setFiles(this.filePath);
+     await fileChooser.setFiles(process.env.LINK_RESUME_PATH!);
      await this.page.waitForTimeout(10000);
 
       const { status, body, url } = await apiPromise;
@@ -102,7 +102,7 @@ async updateProfileHeading(){
 }
    await Utils.click(this.editProfileButton,"Clicked on Profile headline edit icon")
     await Utils.click(this.profileHeadlineTxt, "Clicked on view profile link");
-    await Utils.fill(this.profileHeadlineTxt,this.profileText+await this.getRandomDots(),"Fiiled profile text")
+    await Utils.fill(this.profileHeadlineTxt,process.env.LINK_PROFILE_TEXT+await this.getRandomDots(),"Fiiled profile text")
      await Utils.click(this.saveButton, "Clicked on save button");
     log("Profile heading updated successfully "+ await this.timestamp());
 }
@@ -111,7 +111,7 @@ async updateResumeHeading(){
     log("Updating resume heading...");
     await Utils.click(this.Resumeheadline, "Clicked on resume headline edit icon");
     await Utils.click(this.resumeHeadlineTxt, "Clicked on profile heading input field");
-    await Utils.fill(this.resumeHeadlineTxt, this.ResumeText+await this.getRandomDots(), "Filled Resume heading input field");
+    await Utils.fill(this.resumeHeadlineTxt, process.env.LINK_RESUNE_TEXT+await this.getRandomDots(), "Filled Resume heading input field");
     await Utils.click(this.saveButton, "Clicked on save button");
     log("Resume heading updated successfully "+ await this.timestamp());
 }
