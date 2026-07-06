86e0fb0ff079a61f844713623716878913bc08a2
shreedlondhe <shreedlondhe@gmail.com>
2026-07-06 10:16:30 +0530

    fixed locator issue

--- tests/Linkdin.spec.ts
+++ tests/Linkdin.spec.ts
@@ -18,7 +18,7 @@ let people=await page.locator("//*[contains(@aria-label, 'to connect')]").all()
 console.log("Total People to Connect : "+people.length)
 for(let i = 1; i < people.length; i++){  
     try{
-let person=await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]/../../../a/div/div[2]/div/p[@class='_8d59a5a1 _67dbe9ff acc6e906 a87ae09c _68cdf563 a5b50dc1 _2db943b8 dfa8563b ea9c4e7e']/span`).textContent();
+let person=await page.locator(`((//*[contains(@aria-label, 'to connect')])[${i}]/../../../a//span)[1]`).textContent();
 
 await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]`).click();
 await page.waitForTimeout(1000)
@@ -41,7 +41,7 @@ let people=await page.locator("//*[contains(@aria-label, 'to connect')]").all()
 console.log("Total People to Connect : "+people.length)
 for(let i = 1; i < people.length; i++){  
     try{
-let person=await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]/../../../a/div/div[2]/div/p[@class='_8d59a5a1 _67dbe9ff acc6e906 a87ae09c _68cdf563 a5b50dc1 _2db943b8 dfa8563b ea9c4e7e']/span`).textContent();
+let person=await page.locator(`((//*[contains(@aria-label, 'to connect')])[${i}]/../../../a//span)[1]`).textContent();
 
 await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]`).click();
 await page.waitForTimeout(1000)
@@ -64,7 +64,7 @@ let people=await page.locator("//*[contains(@aria-label, 'to connect')]").all()
 console.log("Total People to Connect : "+people.length)
 for(let i = 1; i < people.length; i++){  
     try{
-let person=await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]/../../../a/div/div[2]/div/p[@class='_8d59a5a1 _67dbe9ff acc6e906 a87ae09c _68cdf563 a5b50dc1 _2db943b8 dfa8563b ea9c4e7e']/span`).textContent();
+let person=await page.locator(`((//*[contains(@aria-label, 'to connect')])[${i}]/../../../a//span)[1]`).textContent();
 
 await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]`).click();
 await page.waitForTimeout(1000)
@@ -87,7 +87,7 @@ let people=await page.locator("//*[contains(@aria-label, 'to connect')]").all()
 console.log("Total People to Connect : "+people.length)
 for(let i = 1; i < people.length; i++){  
     try{
-let person=await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]/../../../a/div/div[2]/div/p[@class='_8d59a5a1 _67dbe9ff acc6e906 a87ae09c _68cdf563 a5b50dc1 _2db943b8 dfa8563b ea9c4e7e']/span`).textContent();
+let person=await page.locator(`((//*[contains(@aria-label, 'to connect')])[${i}]/../../../a//span)[1]`).textContent();
 
 await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]`).click();
 await page.waitForTimeout(1000)
@@ -110,7 +110,7 @@ let people=await page.locator("//*[contains(@aria-label, 'to connect')]").all()
 console.log("Total People to Connect : "+people.length)
 for(let i = 1; i < people.length; i++){  
     try{
-let person=await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]/../../../a/div/div[2]/div/p[@class='_8d59a5a1 _67dbe9ff acc6e906 a87ae09c _68cdf563 a5b50dc1 _2db943b8 dfa8563b ea9c4e7e']/span`).textContent();
+let person=await page.locator(`((//*[contains(@aria-label, 'to connect')])[${i}]/../../../a//span)[1]`).textContent();
 
 await page.locator(`(//*[contains(@aria-label, 'to connect')])[${i}]`).click();
 await page.waitForTimeout(1000)
